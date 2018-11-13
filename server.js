bac9925809aa7ad1c96907a49a2cd29348ba5c4c
carpenter-js <carpenterbarrett@gmail.com>
2018-11-13 14:31:05 -0700

    error

--- server.js
+++ server.js
@@ -32,6 +32,24 @@ app.get('/api/notes/:id', (req, res) => {
   res.json(reqNote);
 });
 
+app.get('/boom', (req, res, next) => {
+  throw new Error('Boom!!');
+});
+
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  res.status(404).json({ message: 'Not Found' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: err
+  });
+});
+
 app.listen(PORT, function () {
   console.info(`Server listening on ${this.address().port}`);
 }).on('error', err => {
