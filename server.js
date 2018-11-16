8b3e126b12edbdd120a8a5e45ba6c97784864d9b
carpenter-js <carpenterbarrett@gmail.com>
2018-11-16 12:49:52 -0700

    added mocha chai and test.server.js

--- server.js
+++ server.js
@@ -39,8 +39,12 @@ app.use(function (err, req, res, next) {
 });
 
 // Listen for incoming connections
-app.listen(PORT, function () {
-  console.info(`Server listening on ${this.address().port}`);
-}).on('error', err => {
-  console.error(err);
-});
+if (require.main === module) {
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
+
+module.exports = app;
